80bf1770fecee406e493a61790e151ca0edcb347
Jsevillamol <Jsevillamol@outlook.com>
2015-04-17 22:16:58 +0200

    Main app function

--- VIDNAV.js
+++ VIDNAV.js
@@ -1,7 +1,7 @@
 
 function main()
 {
-	//{initWaypoint: example, endWaypoint: example, videoURL: example}
+	//{initWaypoint: example, endWaypoint: example, videoURL: example}	
 	
 	function constructWay(initWaypoint, endWaypoint) {
 	//List is a collection of the video IDs
@@ -15,5 +15,46 @@ function main()
 																				//la estación initWaypoint.estacion hasta la estacion endWaypoint.estacion
 		list.push(consult(anden, endWaypoint));*/
 		return urls;
+	}
+	
+	function downloadVideos(urls){
+		var list_of_videos = [];
+		for (int i = 0; i<url.lenght(); i++)
+			list_of_videos.push($.ajax(list[i]));
+		return list_of_videos;
+	}
+	
+	var actual_video = 1;
+	
+	function generateVideos(list_of_videos)
+	{
+		//Consigo el elemento video
+		var videoContainer = document.getElementById("VideoContainer") //TO DO: Apuntar
+		for(int i= 0; i<list_of_videos.lenght(); i++)
+		{
+			var videoTag = createElement("video");
+			videoTag.src = list_of_videos[i];
+			videoTag.setAtributte("id","video"+i);
+			if(i!=0)
+			{
+				videoTag.style.display = "none";
+			}
+			$(VideoContainer).append(videoTag);
+		}
+	}
+	
+	function nextVideo()
+	{
+		var actual = document.getElementById("video"+actual_video');
+		actual.style.display = "none";
+		var actual_video++;
+		var actual = document.getElementById("video"+actual_video');
+		videoTag.style.display = "block";
+	}
+	
+	function startTrayectory(){
+		var urls = constructWay(initWaypoint, endWaypoint);
+		var videos = downloadVideos(urls);
+		var generateVideos (videos);
 	}
 } 
